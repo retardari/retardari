e7236e428405e6eb491130160a7bdab33f3a4f95
real <admin@yuzimu.com>
2019-09-18 18:02:17 +0800

    docs 方案设想

--- net/tcp.go
+++ net/tcp.go
@@ -1,1 +1,39 @@
 package net
+
+import (
+	"net"
+	"sync"
+)
+
+type ConnSet map[net.Conn]struct{}
+
+type TCPServer struct {
+	Addr            string
+	MaxConnNum      int
+	PendingWriteNum int
+	NewAgent        func(*TCPConn) Agent
+	ln              net.Listener
+	conns           ConnSet
+	mutexConns      sync.Mutex
+	wgLn            sync.WaitGroup
+	wgConns         sync.WaitGroup
+
+	LenMsgLen    int
+	MinMsgLen    uint32
+	MaxMsgLen    uint32
+	LittleEndian bool
+	msgParser    *MsgParser
+}
+
+type TCPConn struct {
+}
+
+type TCPClient struct {
+}
+
+type MsgParser struct {
+	lenMsgLen    int
+	minMsgLen    uint32
+	maxMsgLen    uint32
+	littleEndian bool
+}
--- net/udp.go
+++ net/udp.go
@@ -1,1 +1,20 @@
 package net
+
+// udp必须维持心跳 心跳间隔默认未10秒 心跳超时则跑去
+// 数据包的间隔时间为5秒，超时则丢弃
+import (
+	"net"
+	"sync"
+)
+
+type UDPServer struct {
+	Addr            string
+	MaxConnNum      int
+	PendingWriteNum int
+	ln              net.Listener
+	mutexConns      sync.Mutex
+	LenMsgLen       int
+	MinMsgLen       uint32
+	MaxMsgLen       uint32
+	LittleEndian    bool
+}
